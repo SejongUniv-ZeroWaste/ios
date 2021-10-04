ed84d9828c5f4cb11032c7bc52700256bb44050d
iammiori <mioriii@naver.com>
2021-10-04 13:58:51 +0900

    [feat] main vc 초안

---
+++ Zeste/View/Main/MainViewController.swift
@@ -0,0 +1,131 @@
+//
+//  MainViewController.swift
+//  Zeste
+//
+//  Created by miori Lee on 2021/10/03.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MainViewController: BaseViewController {
+    
+    lazy var mainCircleView = CircleView()
+
+    let stampWholeView = StampRoundView()
+    
+    let stampStack = StampStackView()
+    
+    let innerStampView1 = InnerStampStack()
+    
+    let innerStampView2 = InnerStampStack()
+    
+    var myStampCnt : Int = 7
+
+    
+    override func viewDidLayoutSubviews() {
+        mainCircleView.layer.cornerRadius = mainCircleView.frame.height / 2
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        initVC()
+        bindConstraints()
+        setBarButton()
+        getMyStamp()
+        
+    }
+    
+}
+
+extension MainViewController {
+
+    private func setBarButton() {
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            //.viewfinder
+            image: UIImage(systemName: "qrcode"),
+            style: .plain,
+            target: self,
+            action: #selector(qrTapped)
+        )
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "person.crop.circle"),
+            style: .plain,
+            target: self,
+            action: #selector(personTapped)
+        )
+    }
+    
+    private func initVC() {
+        _ = [mainCircleView,stampWholeView].map {self.view.addSubview($0)}
+        _ = [stampStack].map {self.stampWholeView.addSubview($0)}
+        _ = [innerStampView1].map {self.stampStack.stampRound1.addSubview($0)}
+        _ = [innerStampView2].map {self.stampStack.stampRound2.addSubview($0)}
+    }
+    
+    private func bindConstraints() {
+        mainCircleView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(self.view.snp.top).offset(deviceSize.height/4)
+            $0.width.equalTo(self.view.snp.width).multipliedBy(1.57)
+        }
+        stampWholeView.snp.makeConstraints {
+            $0.top.equalTo(mainCircleView.snp.bottom).offset(28)
+            $0.centerX.equalToSuperview()
+            $0.leadingMargin.equalTo(10)
+            $0.height.equalTo(stampWholeView.snp.width).multipliedBy(0.5)
+        }
+        stampStack.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.leadingMargin.equalToSuperview().offset(6)
+            $0.topMargin.equalToSuperview().offset(6)
+        }
+        innerStampView1.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        innerStampView2.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+}
+
+extension MainViewController {
+    @objc func qrTapped() {
+        print("qrTapped")
+    }
+    @objc func personTapped(){
+        print("infoTapped")
+        //moveDown()
+        let nextVC = MyInfoViewController()
+        nextVC.modalPresentationStyle = .fullScreen
+        self.present(nextVC, animated: false, completion: nil)
+    }
+
+}
+
+//MARK: - Stamp 적립 관련 함수
+extension MainViewController {
+    func getMyStamp() {
+        // 한줄 다 채워야하는 경우
+        if myStampCnt > 5 {
+            for i in 1...5 {
+                innerStampView1.circleArr[i].image =  UIImage(named: "stampOn")
+            }
+            let secontCnt = myStampCnt - 5
+            for i in 1...secontCnt {
+                innerStampView2.circleArr[i].image =  UIImage(named: "stampOn")
+            }
+        } else {
+            for i in 1...myStampCnt {
+                innerStampView1.circleArr[i].image =  UIImage(named: "stampOn")
+            }
+        }
+    }
+}
+
