3100a5aa4f6b6d8bde551dd49f306f670e2b841d
iammiori <immioriii@gmail.com>
2021-12-01 22:48:25 +0900

    [feat] text model에 닉네임 설명용 label text 저장

---
+++ Zeste/Model/Text/LabelText.swift
@@ -0,0 +1,12 @@
+//
+//  LabelText.swift
+//  Zeste
+//
+//  Created by miori Lee on 2021/12/01.
+//
+
+import Foundation
+
+struct LabelText {
+    let joindes1 : String = "앱에서 사용하실 닉네임을 적어주세요.\n바르고 고운 단어로 사용해주세요 :)"
+}
