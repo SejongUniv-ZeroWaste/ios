6048803d2fbf003f130e116ed21d12706a17e8ef
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2021-12-02 23:37:35 +0900

    [feat] 추가정보 닉네임 - 번호 받아오는 뷰, 로직 완성 (#8)

--- Zeste/Model/Text/LabelText.swift
+++ Zeste/Model/Text/LabelText.swift
@@ -9,4 +9,6 @@ import Foundation
 
 struct LabelText {
     let joindes1 : String = "앱에서 사용하실 닉네임을 적어주세요.\n바르고 고운 단어로 사용해주세요 :)"
+    let joindes2 : String = "본인임을 확인하기 위해 휴대폰 인증을 시작합니다.\n휴대폰 번호는 절대 외부에 공개되지 않습니다."
+    let welcomeT : String =  "제로웨이스트를 향한 당신의 멋진 한걸음, 응원합니다 :)"
 }
--- Zeste/View/Login/GetUserInfoViewController.swift
+++ Zeste/View/Login/GetUserInfoViewController.swift
@@ -34,12 +34,36 @@ class GetUserInfoViewController: BaseViewController {
         $0.text = "\(LabelText().joindes1)"
     }
     
+    let inTF = UITextField().then {
+        $0.placeholder = "닉네임을 입력해주세요"
+        $0.borderStyle = .roundedRect
+        $0.keyboardType = .default
+        $0.autocorrectionType = .no
+        $0.textContentType = .name
+        $0.autocapitalizationType = .none
+        $0.font = UIFont.systemFont(ofSize: 15)
+    }
+    
+    let nextBtn = UIButton().then {
+        $0.setTitle("다음으로", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = .zestGreen
+        $0.layer.cornerRadius = 8
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+    }
+    
+    var cnt : Int = 0
+    var userNick : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        dismissKeyboardWhenTappedAround()
         // Do any additional setup after loading the view.
         initV()
         bindConstraints()
+        
+        nextBtn.addTarget(self, action: #selector(nextTapped(_:)), for: .touchUpInside)
     }
     
 
@@ -47,7 +71,7 @@ class GetUserInfoViewController: BaseViewController {
 
 extension GetUserInfoViewController {
     private func initV() {
-        _ = [pageControl,topicLabel,desLabel].map {self.view.addSubview($0)}
+        _ = [pageControl,topicLabel,desLabel,inTF,nextBtn].map {self.view.addSubview($0)}
     }
     private func bindConstraints() {
         pageControl.snp.makeConstraints {
@@ -62,5 +86,49 @@ extension GetUserInfoViewController {
             $0.leading.equalTo(pageControl)
             $0.top.equalTo(topicLabel.snp.bottom).offset(16)
         }
+        inTF.snp.makeConstraints {
+            $0.top.equalTo(desLabel.snp.bottom).offset(16)
+            $0.leading.equalTo(pageControl)
+            $0.trailing.equalToSuperview().offset(-16)
+        }
+        nextBtn.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.leadingMargin.equalTo(16)
+            $0.bottom.equalToSuperview().offset(-30)
+            $0.height.equalTo(nextBtn.snp.width).multipliedBy(0.1399)
+        }
+    }
+}
+
+extension GetUserInfoViewController {
+    @objc func nextTapped(_ sender : UIButton) {
+        if self.cnt == 0 {
+            finishNick()
+        } else if self.cnt == 1 {
+            finishPhone()
+        } else if self.cnt == 2 {
+            self.changeRootViewController(BaseTabBarController())
+            cnt = 0
+        }
+    }
+    
+    private func finishNick() {
+        userNick = inTF.text!
+        pageControl.currentPage = 1
+        topicLabel.text = "휴대폰 번호"
+        desLabel.text = "\(LabelText().joindes2)"
+        inTF.text = ""
+        inTF.placeholder = "휴대폰 번호를 입력해주세요 (010-0000-0000)"
+        inTF.textContentType = .telephoneNumber
+        inTF.keyboardType = .numberPad
+        cnt += 1
+    }
+    
+    private func finishPhone() {
+        pageControl.currentPage = 2
+        topicLabel.text = "🎉 환영합니다"
+        desLabel.text = "\(userNick)님,\n\(LabelText().welcomeT)"
+        inTF.isHidden = true
+        cnt += 1
     }
 }
