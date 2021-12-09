0a6216f85d2bfed0ad16eca3a3655aac4f1af8de
iammiori <immioriii@gmail.com>
2021-12-09 23:54:06 +0900

    [feat] notification 으로 textfield 가려지는 경우, 키보드 animate 함수 호출

--- Zeste/View/Login/LoginViewController.swift
+++ Zeste/View/Login/LoginViewController.swift
@@ -18,6 +18,7 @@ class LoginViewController: BaseViewController {
         $0.autocorrectionType = .no
         $0.textContentType = .emailAddress
         $0.autocapitalizationType = .none
+        $0.font = UIFont.systemFont(ofSize: 15)
     }
     
     let pwTF = UITextField().then {
@@ -27,6 +28,7 @@ class LoginViewController: BaseViewController {
         $0.autocorrectionType = .no
         $0.textContentType = .password
         $0.isSecureTextEntry = true
+        $0.font = UIFont.systemFont(ofSize: 15)
     }
     
     let loginBtn = UIButton().then {
@@ -47,10 +49,10 @@ class LoginViewController: BaseViewController {
     // real db
     var ref : DatabaseReference!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         initV()
         bindConstraints()
@@ -62,11 +64,27 @@ class LoginViewController: BaseViewController {
         // firebase reference 초기화
         ref = Database.database().reference()
         
+        //_ = [emailTF,pwTF].map {$0.delegate = self}
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        setNavi()
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
 }
 
 extension LoginViewController {
+    private func setNavi() {
+        let navigationBar = navigationController?.navigationBar
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.backgroundColor = .white
+        navigationBarAppearance.shadowColor = .clear
+        navigationBar?.scrollEdgeAppearance = navigationBarAppearance
+    }
     fileprivate func initV(){
         _ = [emailTF,pwTF,loginBtn,joinBtn].map {self.view.addSubview($0)}
     }
@@ -103,13 +121,13 @@ extension LoginViewController {
         // 새로가입
         Auth.auth().createUser(withEmail: in_email, password: in_pw) { [weak self] authResult, error in
             guard let self = self else {return}
-        
+            
             //error handling
             if let error = error {
                 let code = (error as NSError).code
                 switch code {
                 case 17007:
-                // 이미 계정이 있는 경우, 로그인 필요
+                    // 이미 계정이 있는 경우, 로그인 필요
                     self.presentAlert(title: "⚠️ 이미 등록된 계정이 있어요")
                     // 기기에 uid 저장
                 default:
@@ -118,13 +136,14 @@ extension LoginViewController {
                 }
             } else {
                 // error 없는 경우
-                self.changeRootViewController(BaseTabBarController())
+                //self.changeRootViewController(BaseTabBarController())
+                self.navigationController?.pushViewController(GetUserInfoViewController(), animated: false)
                 let userID : String = Auth.auth().currentUser?.uid ?? ""
-                let in_data : UserInfo = UserInfo(nick: "admin", phone: "010-0000-0000", points: 0)
+                let in_data : UserInfo = UserInfo(nick: "miori", phone: "010-0000-0000", points: 0)
                 //self.ref.child("users").child(userID).setValue(["points":0])
                 self.ref.child("users").child(userID).setValue(in_data.toDict)
             }
-
+            
         }
     }
     @objc func loginTapped(_ sender : UIButton) {
@@ -146,4 +165,28 @@ extension LoginViewController {
             }
         }
     }
+}
+
+extension LoginViewController {
+    @objc func keyboardWillShow(_ sender: Notification) {
+        
+        let userInfo: NSDictionary = sender.userInfo! as NSDictionary
+        let keyboardFrame: NSValue = userInfo.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        
+        _ = [emailTF, pwTF].map {if $0.isEditing == true {
+            //print("edit")
+            keyboardAnimate(keyboardRectangle: keyboardRectangle, textField: $0)
+        }}
+        
+        
+    }
+    @objc func keyboardWillHide(_ sender: Notification) {
+        //self.view.frame.origin.y = topbarHeight
+        self.view.transform = .identity
+        
+    }
+    
+    
+    
 }
